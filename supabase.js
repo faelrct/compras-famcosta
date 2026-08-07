ba9047a79455148df823304a00660b15a19d4b76
faelrct <rafaelcosta076@gmail.com>
2026-08-07 19:40:27 +0000

    Correção 1

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// Substitua pelas suas credenciais reais que você pega no painel do Supabase (Project Settings > API)
-const SUPABASE_URL = 'NEXT_PUBLIC_SUPABASE_URL=https://abopaplifnrruoxjfrgn.supabase.co'
-const SUPABASE_ANON_KEY = 'NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_aF9Bv3DasCcCA7s3wDCgDg_GFOldXrw'
+const SUPABASE_URL = 'https://abopaplifnrruoxjfrgn.supabase.co'; // Cole sua URL exata aqui entre as aspas
+const SUPABASE_ANON_KEY = 'sb_publishable_aF9Bv3DasCcCA7s3wDCgDg_GFOldXrw'; // Cole sua chave anon/public aqui entre as aspas
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
